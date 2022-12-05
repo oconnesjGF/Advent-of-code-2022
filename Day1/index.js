c14855e1ad530bc4f25a9347abe292783c8e19f8
oconnesj-GF-Laptop <samuel.oconnell@glidefast.com>
2022-12-05 13:09:48 -0500

    Solution

--- Day1/index.js
+++ Day1/index.js
@@ -3,28 +3,33 @@ var input = document.body.innerText
   .replace(/$\b/gm, "")
   .split(/^\n/gm);
 
-//console.log(input[0].split(/^\b/gm));
+console.log();
 var newArr = [];
-var sum = 0;
-var test = [];
+var sumArr = [];
+const initialValue = 0;
+
+/*the following loop, loops through all the arrays and 
+subarrays and converts the string to int
+*/
 for (var x = 0; x < input.length; x++) {
   newArr.push(input[x].split(/^\b/gm));
-  //console.log(newArr[x])
   for (var y = 0; y < newArr[x].length; y++) {
     if (parseInt(newArr[x][y]) > 0) {
-      console.log(parseInt(newArr[x][y]));
+      if (parseInt(newArr[x][y]) == newArr[x][y]) {
+        newArr[x][y] = parseInt(newArr[x][y]);
+      }
     }
   }
 }
+for (var a = 0; a < newArr.length; a++) {
+  var sumWithInitial = newArr[a].reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  );
+  sumArr.push(sumWithInitial);
+}
+sumArr.sort(function (a, b) {
+  return b - a;
+});
 
-/*
-for(var y = 0; y < newArr.length; y++) {
-  //console.log(parseInt(newArr[0][y]))
-  if(parseInt(newArr[0][y]) > 0){
-    //console.log('true')
-    sum += parseInt(newArr[0][y])
-  }
-  }
-
-  console.log(sum);
-*/
+console.log(sumArr);
